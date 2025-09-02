bf9f4a8fd9effb9e2a8867161b2b7c265d8bc5d6
prathmeshmasurkar8 <103698143+prathmeshmasurkar8@users.noreply.github.com>
2025-09-02 17:36:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,11 +68,17 @@ def run_script():
         f"land_id_zu=0&land_id_ab=0&datum_von={start_date_obj.strftime('%Y-%m-%d')}"
         f"&datum_bis={end_date_obj.strftime('%Y-%m-%d')}&leihe="
     )
-    HEADERS = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-                      "AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/139.0.0.0 Safari/537.36"
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,"
+              "image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.transfermarkt.com/",
+    "Connection": "keep-alive",
     }
+    response = requests.get(BASE_URL, headers=headers, timeout=20)
+
 
     # -------------------- Step 1: Fetch transfer dates --------------------
     print("🌍 Fetching transfer dates from Transfermarkt...", flush=True)
